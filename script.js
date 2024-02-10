94b37dcf60a969d00c712151fccf90f91a6ca3e5
Alexey Andreev <lexandrweb@gmail.com>
2024-02-10 16:51:45 +0300

    fix seminar6

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 // Задание 1
 
-// const Pow = (number) => Math.pow(number, 3)
-// console.log(Pow(2) + Pow(3))
+const Pow = (number) => Math.pow(number, 3)
+console.log(Pow(2) + Pow(3))
 
 // Задание 2
 
-// const taxDeduction = () => {
-//     let userData = prompt('Введите число...')
-//     if(isNaN(userData*1)) alert('значение задано неверно...')
-//     else document.body.innerHTML = `<h1>${userData*0.87}</h1>`
-// }
+const taxDeduction = () => {
+    let userData = prompt('Введите число...')
+    if(isNaN(userData*1)) alert('значение задано неверно...')
+    else document.body.innerHTML = `<h1>${userData*0.87}</h1>`
+}
 
-// taxDeduction()
+taxDeduction()
 
 // Задание 3
 
-// const Minimum = () => {
-//     const num1 = prompt('введите первое число')
-//     const num2 = prompt('введите второе число')
-//     const num3 = prompt('введите третье число')
-//     document.body.innerHTML = `<h1>${Math.min(num1,num2,num3)}</h1>` 
-// }
+const Minimum = () => {
+    const num1 = prompt('введите первое число')
+    const num2 = prompt('введите второе число')
+    const num3 = prompt('введите третье число')
+    document.body.innerHTML = `<h1>${Math.min(num1,num2,num3)}</h1>` 
+}
 
-// Minimum()
+Minimum()
 
 // Задание 4
 
