28a2cbfefe9b05003f9922eaa5829a3a619827e6
Alexey Andreev <lexandrweb@gmail.com>
2024-02-16 00:38:56 +0300

    lesson8

--- script.js
+++ script.js
@@ -1,40 +1,47 @@
-// Задание 1
+// #1
+// for(let i = 0; i <= 10; i++) {
+//     if(i === 0) console.log(`${i} - это ноль`)
+//     else if(i % 2 !== 0) console.log(`${i} - нечетное число`)
+//     else console.log(`${i} - четное число`)
+// }
 
-const Pow = (number) => Math.pow(number, 3)
-console.log(Pow(2) + Pow(3))
+// #2
+// const arr = [1,2,3,4,5,6,7]
+// arr.splice(3,2)
+// console.log(arr)
 
-// Задание 2
+// #3
+// const arr2 = []
+// for (let i = 0; i <= 4; i++) {
+//     arr2.push(Math.floor(Math.random() * 10))
+// }
+// console.log(arr2)
 
-const taxDeduction = () => {
-    let userData = prompt('Введите число...')
-    if(isNaN(userData*1)) alert('значение задано неверно...')
-    else document.body.innerHTML = `<h1>${userData*0.87}</h1>`
+const display = document.querySelector(".display");
+const size = Number(prompt("введите размер башенки, не больше 20"));
+if (isNaN(size) || size > 20) {
+  alert("Введено неправильное значение...");
+  location.reload();
 }
 
-taxDeduction()
+const createCells = (size) => {
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            display.insertAdjacentElement('beforeend',document.createElement('div'))
+            if(j <= i) {
+                display.lastElementChild.style.background = '#eee123'
+            }
+        }
+    }
+    const cubes = display.querySelectorAll('div')
+    cubes.forEach(i => i.classList.add('cube'))
+    console.log(cubes)
+    positionCells(display,size)
+};  
 
-// Задание 3
-
-const Minimum = () => {
-    const num1 = prompt('введите первое число')
-    const num2 = prompt('введите второе число')
-    const num3 = prompt('введите третье число')
-    document.body.innerHTML = `<h1>${Math.min(num1,num2,num3)}</h1>` 
+const positionCells = (el,size) => {
+    el.style.gridTemplateColumns = `repeat(${size},20px`
+    el.style.gridTemplateRows = `repeat(${size},20px`
 }
 
-Minimum()
-
-// Задание 4
-
-const sum = (num1,num2) =>  num1 + num2
-
-const difference = (num1,num2) => num1 > num2 ? num1 - num2 : num2 - num1
-
-const multiplication = (num1,num2) => num1 * num2
-
-const division = (num1,num2) => num1 / num2
-
-console.log(sum(3,5))
-console.log(difference(-5,5))
-console.log(multiplication(3,5))
-console.log(division(-3,5))
+createCells(size)
